b8cc46229ff06343177a9fb718a1ab6c0b73f503
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2024-01-03 03:46:24 +0800

    command: 修复变量错误

--- src/main/java/io/github/silvigarabis/esplitter/MainCommandExecutor.java
+++ src/main/java/io/github/silvigarabis/esplitter/MainCommandExecutor.java
@@ -14,7 +14,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import.org.bukkit.Bukkit;
+import org.bukkit.Bukkit;
 
 import java.util.logging.Logger;
 
