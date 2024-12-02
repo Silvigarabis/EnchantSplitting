b195765bfa118657101478274702bab5f2b176a2
Silvigarabis <silvigarabis@outlook.com>
2024-12-02 10:27:51 +0800

    simplfy Plugin startup

--- src/main/java/io/github/silvigarabis/esplitter/ESplitterPlugin.java
+++ src/main/java/io/github/silvigarabis/esplitter/ESplitterPlugin.java
@@ -10,33 +10,19 @@
 
 package io.github.silvigarabis.esplitter;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.util.logging.Logger;
-import java.io.File;
 
 public final class ESplitterPlugin extends JavaPlugin {
     
-    private boolean debugMode = false;
-    
-    protected void setDebugMode(boolean enable){
-        debugMode = enable;
+    public static ESplitterPlugin getPluginInstance(){
+        if (pluginInstance == null) {
+            throw new RuntimeException("plugin instance not found");
+        }
+        return pluginInstance;
     }
-    
-    public boolean isDebugMode(){
-        return debugMode;
-    }
-    
-    public static ESplitterPlugin getPlugin(){
-        return plugin;
-    }
-    private static ESplitterPlugin plugin = null;
+    private static ESplitterPlugin pluginInstance = null;
     
     private Logger logger;
 
@@ -46,27 +32,35 @@ public final class ESplitterPlugin extends JavaPlugin {
 
         logger.info("ESplitter 正在加载。");
         
-        if (plugin == null){
-            plugin = this;
+        if (pluginInstance == null){
+            pluginInstance = this;
         } else {
-            logger.severe("检测到另一个插件实例正在运行！");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
+            logger.severe("插件状态异常");
+            throw new IllegalStateException("plugin is not null");
         }
 
-        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
-        this.getCommand("esplitter").setExecutor(new MainCommandExecutor());
+        registerEvents(this);
+        registerCommands(this);
         
         logger.info("ESplitter 已加载。");
         logger.info("ESplitter 插件，一个让玩家可以分离装备上的附魔的插件");
         logger.info("源代码： https://github.com/Imeaces/EnchantmentSplitter");
         logger.info("你可以在在GitHub上提出建议，或者反馈错误： https://github.com/Imeaces/EnchantmentSplitter/issues");
     }
+
+    private static void registerEvents(ESplitterPlugin plugin){
+        plugin.getServer().getPluginManager().registerEvents(new ESplitterListener(), plugin);
+    }
+
+    private static void registerCommands(ESplitterPlugin plugin){
+        plugin.getCommand("esplitter").setExecutor(new MainCommandExecutor());
+    }
+    
     
     @Override
     public void onDisable() {
-        if (plugin == this){
-            plugin = null;
+        if (this.equals(pluginInstance)){
+            pluginInstance = null;
         }
         logger.info("插件已禁用");
     }
